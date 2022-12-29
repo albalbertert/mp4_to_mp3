0b35c3c7f0844468f584fd72789f449b9aff4701
Albert <albert.asatryan.99@mail.ru>
2022-12-30 01:51:17 +0400

    add_function

--- video_to_audio.py
+++ video_to_audio.py
@@ -3,14 +3,19 @@ from moviepy.editor import VideoFileClip, AudioFileClip
 from pathlib import Path
 
 
-video_path = Path(path_of_video)  # path_of_video
+video_path = Path(r'your_path_to_video')
 
-# 1-st variant
-# audio = AudioFileClip(f"{video_path}")
 
-# 2-nd variant
-video = VideoFileClip(f"{video_path}")
-audio = video.audio
+def video_to_audio(path):
+    # 1-st variant
+    # audio = AudioFileClip(f"{video_path}")
 
-audio.write_audiofile(f"{video_path.stem}.mp3", buffersize=2000)  # wright audio
+    # 2-nd variant
+    video = VideoFileClip(f"{path}")
+    audio = video.audio
 
+    audio.write_audiofile(f"{path.stem}.mp3", buffersize=2000)  # wright audio
+
+
+if __name__ == '__main__':
+    video_to_audio(video_path)
